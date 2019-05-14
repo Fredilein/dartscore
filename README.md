96501073d2f876c71bb13d94e47bf10147c45cfa
Adrian Zanga <adrian.zanga@icloud.com>
2019-05-14 17:55:16 +0200

    add mongoose

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # dartscore
 
-## Start server and client
+## Setup
+Create a file `credential.js` in the root directory of this project with your database login:
+```
+module.exports = {
+  DB_SERVER: "<server>",
+  DB_USER: "<username>",
+  DB_PASS: "<password>"
+}
+```
+
+# Start server and client
 ```
 yarn start:server
 yarn start:client
--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const creds = require('../credentials.js');
+
+
+
+// Setup DB
+mongoose.connect(`mongodb://${creds.DB_USER}:${creds.DB_PASS}@${creds.DB_SERVER}/dartscore`, {useNewUrlParser: true});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to db');
+});
+
 
 // Create express app instance:
 const app = express();
@@ -13,10 +25,10 @@ io.on('connection', function(socket) {
   console.log(socket.id);
 
   socket.on('SEND_TURNSCORE', function(data) {
-    io.emit('TURNSCORE', data)
+    io.emit('TURNSCORE', data);
   });
 
   socket.on('CLEAR_SCORES', function() {
-    io.emit('CLEAR_SCORES')
+    io.emit('CLEAR_SCORES');
   });
 });
