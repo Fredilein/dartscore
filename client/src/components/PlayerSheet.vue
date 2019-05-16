290926326bc0ac2dfd2145fc3b18adea6a0a7f9b
Adrian Zanga <adrian.zanga@icloud.com>
2019-05-16 14:58:28 +0200

    add playersheet

---
+++ client/src/components/PlayerSheet.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h5>{{ player.name }}</h5>
+    <p>Remaining: {{ player.remaining }}</p>
+    <p>Legs: {{ player.legs }}</p>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'PlayerSheet',
+  props: {
+    player: {
+      name: String,
+      remaining: Number,
+      legs: Number
+    }
+  }
+}
+</script>
+
+
+
+<style lang="stylus">
+
+
+</style>
